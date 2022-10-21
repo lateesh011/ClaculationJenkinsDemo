a8c3c18aae99a1d894e641e29df05dc8c6cd19c8
username <useremail@cognizant.com>
2022-10-21 09:30:48 +0530

    Add project files.

---
+++ ClaculationJenkinsDemo/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaculationJenkinsDemo
+{
+    public class Class1
+    {
+        public class Calculator
+        {
+            public int Add(int i, int j)
+            {
+                return i + j;
+            }
+            public int Sub(int i, int j)
+            {
+                return i - j;
+            }
+            public int Mul(int i, int j)
+            {
+                return i * j;
+            }
+        }
+    }
+}
